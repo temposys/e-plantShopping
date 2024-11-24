c0ed563f2b038b133e35545902952d4712e119c7
iana.sergieieva <iana.sergieieva@bahnhof.net>
2024-11-24 14:18:22 -0800

    add dispatch

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -2,7 +2,9 @@ import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 import {addItem} from "./CartSlice.jsx";
+import {useDispatch} from "react-redux";
 function ProductList() {
+    const dispatch = useDispatch();
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
